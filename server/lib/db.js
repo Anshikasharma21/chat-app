2fb22b359ee2b904e43085c735098a0e3b5c3038
Anshika Sharma <anshikasharma5697@gmail.com>
2026-06-14 20:45:53 +0530

    fix backend react import issue

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,12 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        mongoose.connection.on("connected", () => {
-            console.log("Database Connected");
-        });
-
         await mongoose.connect(process.env.MONGODB_URI);
-
+        console.log("Database Connected");
     } catch (error) {
         console.log("MongoDB connection error:", error.message);
         process.exit(1);
